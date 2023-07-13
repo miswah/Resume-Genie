510d0ecf0436f405d127bc51dfc4044163557913
miswah <miswah.md@gmail.com>
2023-07-13 23:18:40 +0530

    Initial starter files

---
+++ src/test/java/io/miswah/resumebuilder/ResumebuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.miswah.resumebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumebuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
