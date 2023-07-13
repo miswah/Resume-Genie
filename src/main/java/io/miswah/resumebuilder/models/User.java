283e6976618ae0e4fa909e19373cc43bf34dad78
miswah <miswah.md@gmail.com>
2023-07-13 23:37:58 +0530

    Add User Model

---
+++ src/main/java/io/miswah/resumebuilder/models/User.java
@@ -0,0 +1,77 @@
+package io.miswah.resumebuilder.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "userName")
+    private String userName;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "roles")
+    private String roles;
+
+
+    //Constructors
+    public User() {
+    }
+
+    public User(String userName, String password, boolean active, String roles) {
+        this.userName = userName;
+        this.password = password;
+        this.active = active;
+        this.roles = roles;
+    }
+
+    //Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}
