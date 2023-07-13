5a5288edabeaa97d334a8c3e89ccb6cf0fdba952
miswah <miswah.md@gmail.com>
2023-07-13 23:39:55 +0530

    Add User Repository

---
+++ src/main/java/io/miswah/resumebuilder/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package io.miswah.resumebuilder.repository;
+
+import io.miswah.resumebuilder.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
