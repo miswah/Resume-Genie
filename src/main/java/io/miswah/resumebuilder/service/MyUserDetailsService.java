d6c504f464dd39162af9aae039b7b505536ec9e6
miswah <miswah.md@gmail.com>
2023-07-13 23:56:23 +0530

    Add service and model for userDetails

---
+++ src/main/java/io/miswah/resumebuilder/service/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package io.miswah.resumebuilder.service;
+
+import io.miswah.resumebuilder.models.User;
+import io.miswah.resumebuilder.models.MyUserDetails;
+import io.miswah.resumebuilder.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        return user.map(MyUserDetails::new).get();
+    }
+}
