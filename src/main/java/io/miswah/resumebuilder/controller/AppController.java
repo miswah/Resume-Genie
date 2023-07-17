e74a091a11be9e1197d61e3559ed23e7caa8d06e
miswah <miswah.md@gmail.com>
2023-07-17 22:52:46 +0530

    (fix): typo

--- src/main/java/io/miswah/resumebuilder/controller/AppController.java
+++ src/main/java/io/miswah/resumebuilder/controller/AppController.java
@@ -1,14 +1,17 @@
 package io.miswah.resumebuilder.controller;
 
 
+
 import io.miswah.resumebuilder.models.UserProfile;
 import io.miswah.resumebuilder.repository.UserProfileRepository;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.Optional;
 
@@ -42,4 +45,6 @@ public class AppController {
         model.addAttribute("userProfile", userProfile.get());
         return "resume-templates/"+userProfile.get().getSelectedTemplate()+"/index";
     }
+
+
 }
