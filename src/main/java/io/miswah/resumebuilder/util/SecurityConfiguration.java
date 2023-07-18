284ce059412bfb4423148fe5c3739fe8a66a4c08
miswah <miswah.md@gmail.com>
2023-07-18 22:09:01 +0530

    (fix): remove /error from security filter

--- src/main/java/io/miswah/resumebuilder/util/SecurityConfiguration.java
+++ src/main/java/io/miswah/resumebuilder/util/SecurityConfiguration.java
@@ -1,10 +1,9 @@
 package io.miswah.resumebuilder.util;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +22,7 @@ public class SecurityConfiguration  {
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(new AntPathRequestMatcher("/view/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/resume-resources/**")).permitAll() //enable static resource access for resume-resources folder
+                        .requestMatchers(new AntPathRequestMatcher("/error")).permitAll()
                         .anyRequest().authenticated())
                 .formLogin(withDefaults());
 
