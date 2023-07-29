9852edd3f205bc8e295c193a43dd3586012b38b8
miswah <miswah.md@gmail.com>
2023-07-29 21:32:33 +0530

    (enh): replaced h2 with mariadb

--- target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-insert into _user (id, user_name, password, active, roles) values
-(1, 'adam', 'apple', true, 'USER');
-
---insert into user_profile (id, selected_template, summary, user_name, first_name, last_name, current_designation,email, phone) values
---(1, 1, '   maecenas. Elit deterruisset honestatis fringilla pulvinar felis unum hinc vocibus. Consetetur percipit risus augue omnesque elitr. Mei eripuit signiferumque iuvaret erroribus duo massa omnesque. Dissentiunt interpretaris nominavi natoque pericula venenatis delectus. Laoreet praesent mazim turpis intellegebat. Quam eget fermentum tempus dicant duo natoque pro. Propriae aliquet equidem oporteat mattis eruditi. Finibus quis ultrices repudiandae sapie', 'adam', 'adam', 'jesus', 'flying monster', 'flyingmoster@gmail.com', '+91 969696969');
-
